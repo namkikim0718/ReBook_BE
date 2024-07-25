9c51e12e5133770a7e9c7e7c26eef776d9973680
Austin-Choi <austinsupra@gmail.com>
2024-07-25 23:01:39 +0900

    fix : 회원 가입 로직에서 파라미터 못 찾는 문제 해결

--- src/main/java/com/be/rebook/domain/members/controller/JoinController.java
+++ src/main/java/com/be/rebook/domain/members/controller/JoinController.java
@@ -7,6 +7,7 @@ import com.be.rebook.global.config.BaseResponse;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
@@ -22,7 +23,7 @@ public class JoinController {
     }
 
     @PostMapping("/join")
-    public ResponseEntity<BaseResponse<Members>> joinProcess(JoinDTO joinDTO) {
+    public ResponseEntity<BaseResponse<Members>> joinProcess(@RequestBody JoinDTO joinDTO) {
         joinLogger.info(joinDTO.getUsername());
         return ResponseEntity.ok().body(new BaseResponse<>(joinService.joinProcess(joinDTO)));
     }
