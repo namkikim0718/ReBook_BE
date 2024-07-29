6840932bdb5a268a7525b6b1866e82225ddb360e
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-07-29 17:34:33 +0900

    feat: Product 관련 API 구현

---
+++ src/main/java/com/be/rebook/domain/product/controller/ProductController.java
@@ -0,0 +1,76 @@
+package com.be.rebook.domain.product.controller;
+
+import com.be.rebook.domain.product.service.ProductService;
+import com.be.rebook.global.config.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.be.rebook.domain.product.dto.ProductRequestDTO.*;
+import static com.be.rebook.domain.product.dto.ProductResponseDTO.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ProductController {
+
+    private final ProductService productService;
+
+    /**
+     * 상품 등록
+     */
+    @PostMapping("/members/{memberId}/product")
+    public ResponseEntity<BaseResponse<Long>> createProduct(@PathVariable("memberId") Long memberId,
+                                                            @RequestPart("productRequest") ProductSaveRequestDTO productSaveRequestDTO,
+                                                            @RequestPart("imageFiles") List<MultipartFile> imageFiles) throws IOException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new BaseResponse<>(
+                HttpStatus.CREATED,
+                "201 CREATED",
+                "상품이 등록되었습니다.",
+                productService.createProduct(memberId, productSaveRequestDTO, imageFiles)));
+    }
+
+    /**
+     * 상품 목록 조회
+     */
+    @GetMapping("/products")
+    public ResponseEntity<BaseResponse<List<ProductListResponseDTO>>> findAllProduct(
+            @RequestParam(required = false) String university,
+            @RequestParam(required = false) String major,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) String order) {
+        return ResponseEntity.ok().body(new BaseResponse<>(productService.findAllProductByFilter(university, major, title, minPrice, maxPrice, order)));
+    }
+
+    /**
+     * 상품 단건 조회
+     */
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<BaseResponse<ProductDetailResponseDTO>> findProductById(@PathVariable("productId") Long productId) {
+        return ResponseEntity.ok().body(new BaseResponse<>(productService.findProductById(productId)));
+    }
+
+    /**
+     * 상품 상태 변경
+     */
+    @PatchMapping("/products/{productId}")
+    public ResponseEntity<BaseResponse<Long>> changeProductStatus(@PathVariable("productId") Long productId, @RequestBody ProductStatusRequestDTO productStatusRequestDTO) {
+        return ResponseEntity.ok().body(new BaseResponse<>(productService.changeStatus(productId, productStatusRequestDTO.getStatus())));
+    }
+
+    /**
+     * 상품 삭제
+     */
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<BaseResponse<String>> deleteProductById(@PathVariable("productId") Long productId) {
+        return ResponseEntity.ok().body(new BaseResponse<>(productService.deleteById(productId)));
+    }
+
+}
--- src/main/java/com/be/rebook/domain/product/service/ProductService.java
+++ src/main/java/com/be/rebook/domain/product/service/ProductService.java
@@ -2,27 +2,41 @@ package com.be.rebook.domain.product.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.be.rebook.domain.members.entity.Members;
+import com.be.rebook.domain.members.repository.MembersRepository;
+import com.be.rebook.domain.product.domain.Product;
 import com.be.rebook.domain.product.dto.ProductRequestDTO;
+import com.be.rebook.domain.product.dto.ProductResponseDTO;
 import com.be.rebook.domain.product.repository.ProductRepository;
+import com.be.rebook.domain.productImage.domain.ProductImage;
 import com.be.rebook.domain.productImage.repository.ProductImageRepository;
+import com.be.rebook.global.exception.BaseException;
+import com.be.rebook.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static com.be.rebook.domain.product.dto.ProductResponseDTO.*;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class ProductService {
 
     private final ProductImageRepository productImageRepository;
     private final AmazonS3Client amazonS3Client;
     private final ProductRepository productRepository;
-//    private final MemberRopository memberRopository;
+    private final MembersRepository membersRepository;
 
     /**
      *  S3
@@ -33,36 +47,82 @@ public class ProductService {
 
     // TODO -> 이미지파일 업로드 후 product 엔티티 저장
     @Transactional
-    public Long createProduct(Long memberId, ProductRequestDTO.ProductRequest productRequest, MultipartFile imageFile) throws IOException {
+    public Long createProduct(Long memberId, ProductRequestDTO.ProductSaveRequestDTO productSaveRequestDTO, List<MultipartFile> imageFiles) throws IOException {
 
-//        Member member = memberRepository.findById(memberId)
-//                .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND));
+        Members member = membersRepository.findById(memberId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NO_USER_INFO));
 
-        // 원본 파일명
-        String originalFileName = imageFile.getOriginalFilename();
-        // 저장 파일명
-        String storeFileName = createStoreFileName(originalFileName);
+        // Product 먼저 저장
+        Product product = Product.of(member, productSaveRequestDTO);
+        Long savedProductId = productRepository.save(product).getId();
 
-        // S3에 저장
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(imageFile.getContentType());
-        metadata.setContentLength(imageFile.getSize());
-        amazonS3Client.putObject(bucket, storeFileName, imageFile.getInputStream(), metadata);
+        // 각 이미지 파일을 S3에 업로드 한 후 prductImage로 저장(Product에도 추가)
+        for (MultipartFile imageFile : imageFiles) {
+            // 원본 파일명
+            String originalFileName = imageFile.getOriginalFilename();
+            // 저장 파일명
+            String storeFileName = createStoreFileName(originalFileName);
 
-//        Product product = productRepository.findById(productId)
-//                .orElseThrow(() -> new BaseException(ErrorCode.NOT_EXIST_PRODUCT));
+            // S3에 저장
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(imageFile.getContentType());
+            metadata.setContentLength(imageFile.getSize());
+            amazonS3Client.putObject(bucket, storeFileName, imageFile.getInputStream(), metadata);
 
+            ProductImage productImage = ProductImage.builder()
+                    .uploadFileName(originalFileName)
+                    .storeFileName(storeFileName)
+                    .product(product)
+                    .build();
 
-//        ProductImage productImage = ProductImage.builder()
-//                .uploadFileName(originalFileName)
-//                .storeFileName(storeFileName)
-//                .product(product)
-//                .build();
-//
-//        ProductImage savedProductImage = productImageRepository.save(productImage);
-//        return savedProductImage.getId();
-        return 1L;
+            productImageRepository.save(productImage);
+        }
+
+        return savedProductId;
     }
+
+    /**
+     * 상품 목록 조회
+     */
+    public List<ProductListResponseDTO> findAllProductByFilter(String university, String major, String title, Integer minPrice, Integer maxPrice, String order) {
+        List<Product> products = productRepository.findProductsByFilter(university, major, title, minPrice, maxPrice, order);
+
+        return products.stream()
+                .map(ProductListResponseDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 상품 단건 조회
+     */
+    public ProductDetailResponseDTO findProductById(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOT_EXIST_PRODUCT));
+
+        return new ProductDetailResponseDTO(product);
+    }
+
+    /**
+     * 상품 상태 변경
+     */
+    @Transactional
+    public Long changeStatus(Long productId, String status) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOT_EXIST_PRODUCT));
+
+        product.changeStatus(status);
+        return productId;
+    }
+
+    /**
+     * 상품 삭제
+     */
+    @Transactional
+    public String deleteById(Long productId) {
+        productRepository.deleteById(productId);
+        return "상품 삭제 완료";
+    }
+
 
     /**
      * 파일명이 겹치는 것을 방지하기위해 중복되지않는 UUID를 생성해서 반환(ext는 확장자)
