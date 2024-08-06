405d3341443604bb721ef046910040ff5b82ec9e
Austin-Choi <austinsupra@gmail.com>
2024-08-06 09:00:07 +0900

    refactor : rebook-security 모듈 SecurityApplication 추가

---
+++ rebook-security/src/main/java/com/be/rebook/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package com.be.rebook.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableScheduling
+public class SecurityApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
