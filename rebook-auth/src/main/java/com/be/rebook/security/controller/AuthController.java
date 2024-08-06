275fd4e3d3323f97d81904d544eabb7c03f61a87
Austin-Choi <austinsupra@gmail.com>
2024-08-06 17:25:26 +0900

    patch : auth RequestMapping 추가

--- rebook-auth/src/main/java/com/be/rebook/security/controller/AuthController.java
+++ rebook-auth/src/main/java/com/be/rebook/security/controller/AuthController.java
@@ -13,11 +13,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
 
     private static final Logger joinLogger = LoggerFactory.getLogger(AuthController.class);
