9d0f544dd93226e06f8577fe30e3a8783663da04
kimjaehyuk <jhkim0097@gmail.com>
2024-08-07 17:05:27 +0900

    [feat] dotevn 라이브러리로 .env 환경변수 참조 #26

--- rebook-auth/src/main/java/com/be/rebook/auth/SecurityApplication.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/SecurityApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
 public class SecurityApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(SecurityApplication.class, args);
     }
 }
