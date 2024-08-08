eb21c968f9ac8f0cdf63775e7281011918681598
kimjaehyuk <jhkim0097@gmail.com>
2024-08-08 19:49:36 +0900

    [fix] 인증 엔드포인트 추가 #26

--- rebook-auth/src/main/java/com/be/rebook/auth/controller/AuthController.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.be.rebook.auth.controller;
 
-
 import com.be.rebook.auth.entity.Members;
 import com.be.rebook.auth.entity.RefreshTokens;
+import com.be.rebook.auth.dto.CustomUserDetails;
 import com.be.rebook.auth.dto.SignupDTO;
 import com.be.rebook.auth.service.ReissueService;
 import com.be.rebook.auth.service.SignupService;
 import com.be.rebook.common.config.BaseResponse;
+import com.be.rebook.common.exception.BaseException;
+import com.be.rebook.common.exception.ErrorCode;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,11 +16,14 @@ import jakarta.validation.Valid;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import org.slf4j.Logger;
 
@@ -32,7 +37,7 @@ public class AuthController {
     private final ReissueService reissueService;
 
     public AuthController(SignupService signupService,
-                          ReissueService reissueService){
+            ReissueService reissueService) {
         this.signupService = signupService;
         this.reissueService = reissueService;
     }
@@ -44,7 +49,22 @@ public class AuthController {
     }
 
     @PostMapping("/members/refreshtoken")
-    public ResponseEntity<BaseResponse<RefreshTokens>> reissue(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<BaseResponse<RefreshTokens>> reissue(HttpServletRequest request,
+            HttpServletResponse response) {
         return ResponseEntity.ok().body(new BaseResponse<>(reissueService.reissueToken(request, response)));
+    }
+
+    @PostMapping("/members/authenticate")
+    public ResponseEntity<BaseResponse<CustomUserDetails>> authenticate() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof CustomUserDetails) {
+            CustomUserDetails userDetails = (CustomUserDetails) principal;
+            // 필요한 데이터를 DTO로 변환
+            return ResponseEntity.ok().body(new BaseResponse<>(userDetails));
+        } else {
+            throw new BaseException(ErrorCode.UNAUTHORIZED); // TODO: 적절한 Exception 처리
+        }
     }
 }
