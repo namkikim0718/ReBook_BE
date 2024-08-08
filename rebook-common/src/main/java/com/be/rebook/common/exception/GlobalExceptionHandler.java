8bcd1ef8ece8f3732a460f4987b55e37f13363b7
kimjaehyuk <jhkim0097@gmail.com>
2024-08-08 16:30:40 +0900

    [style] 미사용 import 제거 #26

--- rebook-common/src/main/java/com/be/rebook/common/exception/GlobalExceptionHandler.java
+++ rebook-common/src/main/java/com/be/rebook/common/exception/GlobalExceptionHandler.java
@@ -5,16 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.be.rebook.common.config.BaseResponse;
 
 @RestControllerAdvice
 @Slf4j
