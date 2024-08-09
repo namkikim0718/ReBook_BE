871e0e96fb857c7f2b1d5db2aa2ae6bea9be8e9d
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-09 15:50:00 +0900

    [refactor] s3관련 로직 common으로 분리

--- rebook-product/src/main/java/com/be/rebook/product/service/ProductService.java
+++ rebook-product/src/main/java/com/be/rebook/product/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.be.rebook.product.service;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.be.rebook.common.service.S3Service;
 import com.be.rebook.product.entity.Product;
 import com.be.rebook.product.dto.ProductRequestDTO;
 import com.be.rebook.product.repository.ProductRepository;
@@ -11,14 +10,12 @@ import com.be.rebook.common.exception.BaseException;
 import com.be.rebook.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static com.be.rebook.product.dto.ProductResponseDTO.*;
@@ -30,14 +27,9 @@ import static com.be.rebook.product.dto.ProductResponseDTO.*;
 public class ProductService {
 
     private final ProductImageRepository productImageRepository;
-    private final AmazonS3Client amazonS3Client;
     private final ProductRepository productRepository;
+    private final S3Service s3Service;
 
-    /**
-     *  S3
-     */
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
 
     @Transactional
     public Long createProduct(Long sellerId,
@@ -51,19 +43,11 @@ public class ProductService {
 
         // 각 이미지 파일을 S3에 업로드 한 후 prductImage로 저장(Product에도 추가)
         for (MultipartFile imageFile : imageFiles) {
-            // 원본 파일명
-            String originalFileName = imageFile.getOriginalFilename();
-            // 저장 파일명
-            String storeFileName = createStoreFileName(originalFileName);
 
-            // S3에 저장
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(imageFile.getContentType());
-            metadata.setContentLength(imageFile.getSize());
-            amazonS3Client.putObject(bucket, storeFileName, imageFile.getInputStream(), metadata);
+            String storeFileName = s3Service.uploadFile(imageFile);
 
             ProductImage productImage = ProductImage.builder()
-                    .uploadFileName(originalFileName)
+                    .uploadFileName(imageFile.getOriginalFilename())
                     .storeFileName(storeFileName)
                     .product(product)
                     .build();
@@ -130,23 +114,5 @@ public class ProductService {
     public String deleteById(Long productId) {
         productRepository.deleteById(productId);
         return "상품 삭제 완료";
-    }
-
-
-    /**
-     * 파일명이 겹치는 것을 방지하기위해 중복되지않는 UUID를 생성해서 반환(ext는 확장자)
-     */
-    private String createStoreFileName(String originalFilename) {
-        String ext = extractExt(originalFilename);
-        String uuid = UUID.randomUUID().toString();
-        return uuid + "." + ext;
-    }
-
-    /**
-     * 파일 확장자를 추출하기 위해 만든 메서드
-     */
-    private String extractExt(String originalFilename) {
-        int post = originalFilename.lastIndexOf(".");
-        return originalFilename.substring(post + 1);
     }
 }
