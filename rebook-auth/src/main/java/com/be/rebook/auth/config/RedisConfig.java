7d679a9670eb3cf2fafc6314c99ae222963fd83f
Austin-Choi <austinsupra@gmail.com>
2024-08-10 06:55:54 +0900

    feature : redis 설정 수정

--- rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
@@ -1,21 +1,42 @@
 package com.be.rebook.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 // todo : common에 공통모듈로 인터페이스화해서 올리기
 @Configuration
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    // Redis와의 연결을 위한 connection 생성
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        //Redis 연결
+        template.setConnectionFactory(redisConnectionFactory());
+
+        //key-value 형태로 직렬화를 수행
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
+
+        //기본적 직렬화
+        template.setDefaultSerializer(new StringRedisSerializer());
+
         return template;
     }
 }
