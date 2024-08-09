12f7a5b152e17a61a2098d0ac72c835d1eb2656d
kimjaehyuk <jhkim0097@gmail.com>
2024-08-10 01:33:49 +0900

    chore: Add Eureka Server application class

---
+++ eureka-server/src/main/java/com/be/rebook/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.be.rebook.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//공통 의존성때문에 DataSourceAutoConfiguration을 제외해야함
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
