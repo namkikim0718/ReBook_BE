8e51ff2bd95527e298b66e89f831d31be48f0a3b
Austin-Choi <austinsupra@gmail.com>
2024-08-09 13:44:38 +0900

    feature : 사용자에게 이메일 보내는 EmailService 추가

---
+++ rebook-auth/src/main/java/com/be/rebook/auth/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.be.rebook.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendVerificationEmail(String to, String verificationCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("ReBook 가입 인증 코드입니다.");
+        message.setText("\n" + verificationCode);
+        mailSender.send(message);
+    }
+}
