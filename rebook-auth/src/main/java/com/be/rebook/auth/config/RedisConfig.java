2293b590baab7c3fdd8eb122445d545d63e7acba
Austin-Choi <austinsupra@gmail.com>
2024-08-09 17:45:46 +0900

    feature : Redis 설정하는 클래스 추가

--- rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+// todo : common에 공통모듈로 인터페이스화해서 올리기
 @Configuration
 public class RedisConfig {
 
