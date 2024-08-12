22cce5ac8b3dbcb7ed1581be36f67bcf965af375
kimjaehyuk <jhkim0097@gmail.com>
2024-08-12 16:32:23 +0900

    [feat] socket io module init #38

---
+++ rebook-chat/src/main/java/com/be/rebook/chat/ChatApplication.java
@@ -0,0 +1,19 @@
+package com.be.rebook.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.be.rebook.common", "com.be.rebook.chat"})
+public class ChatApplication {
+    public static void main(String[] args) {
+        // Spring Boot 애플리케이션 실행
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
---
+++ rebook-chat/src/main/java/com/be/rebook/chat/config/SocketIOConfig.java
@@ -0,0 +1,24 @@
+package com.be.rebook.chat.config;
+
+@Configuration
+public class SocketIOConfig {
+
+    @Value("${socketio.host}")
+    private String host;
+
+    @Value("${socketio.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        return new SocketIOServer(config);
+    }
+
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketIOServer) {
+        return new SpringAnnotationScanner(socketIOServer);
+    }
+}
