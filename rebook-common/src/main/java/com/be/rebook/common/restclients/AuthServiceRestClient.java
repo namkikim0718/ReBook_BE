985ff22bff648350b70f8132af6ce54188e6d364
kimjaehyuk <jhkim0097@gmail.com>
2024-08-12 15:19:06 +0900

    [fix] 메서드 네임 수정 Get -> Post #30

--- rebook-common/src/main/java/com/be/rebook/common/restclients/AuthServiceRestClient.java
+++ rebook-common/src/main/java/com/be/rebook/common/restclients/AuthServiceRestClient.java
@@ -1,12 +1,14 @@
 package com.be.rebook.common.restclients;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 import com.be.rebook.common.argumentresolver.auth.MemberLoginInfo;
+import com.be.rebook.common.config.BaseResponse;
 
 public interface AuthServiceRestClient {
 
-    @GetExchange("/auth/members/authenticate")
-    MemberLoginInfo authenticate(@RequestHeader("Authorization") String token);
+    @PostExchange("/auth/members/authenticate")
+    ResponseEntity<BaseResponse<MemberLoginInfo>> authenticate(@RequestHeader("Authorization") String token);
 }
