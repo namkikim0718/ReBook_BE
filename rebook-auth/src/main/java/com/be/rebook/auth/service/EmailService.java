427c2e154be7eeb70f86dc971476d01485ae5048
Austin-Choi <austinsupra@gmail.com>
2024-08-12 10:37:26 +0900

    refactor : 필드주입에서 생성자주입으로 변경

--- rebook-auth/src/main/java/com/be/rebook/auth/service/EmailService.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/service/EmailService.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
 
     public void sendVerificationEmail(String to, String verificationCode) {
         SimpleMailMessage message = new SimpleMailMessage();
