2c43dccdb7530a78aff28f62df6ddfd0527547e0
Austin-Choi <austinsupra@gmail.com>
2024-08-12 16:01:05 +0900

    fix : 잘못된 import 문 수정

--- rebook-common/src/main/java/com/be/rebook/common/service/S3Service.java
+++ rebook-common/src/main/java/com/be/rebook/common/service/S3Service.java
@@ -3,7 +3,7 @@ package com.be.rebook.common.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
