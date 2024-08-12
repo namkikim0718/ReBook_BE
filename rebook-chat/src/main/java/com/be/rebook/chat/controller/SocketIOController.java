0eb8be0be1e44de070b74ad1a1cc8fe661054126
kimjaehyuk <jhkim0097@gmail.com>
2024-08-12 19:51:46 +0900

    [feat] 소켓 서버 구현 #38

--- rebook-chat/src/main/java/com/be/rebook/chat/controller/SocketIOController.java
+++ rebook-chat/src/main/java/com/be/rebook/chat/controller/SocketIOController.java
@@ -4,6 +4,7 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.OnConnect;
 import com.corundumstudio.socketio.annotation.OnDisconnect;
 import com.corundumstudio.socketio.annotation.OnEvent;
+
 import org.springframework.stereotype.Component;
 
 @Component
