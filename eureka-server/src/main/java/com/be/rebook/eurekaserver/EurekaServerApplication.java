32b0f34ca77ccbf8d1a292918283cb618b96b079
kimjaehyuk <jhkim0097@gmail.com>
2024-08-12 15:16:40 +0900

    [feat] ComponentScan 추가 #30

--- eureka-server/src/main/java/com/be/rebook/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/be/rebook/eurekaserver/EurekaServerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 //공통 의존성때문에 DataSourceAutoConfiguration을 제외해야함
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableEurekaServer
+@ComponentScan(basePackages = {"com.be.rebook.eurekaserver"})
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
