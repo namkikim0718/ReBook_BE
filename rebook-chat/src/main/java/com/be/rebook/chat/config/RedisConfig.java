ee5da66f70c728f2a69b2f875e338154d02df0f1
kimjaehyuk <jhkim0097@gmail.com>
2024-08-13 18:30:24 +0900

    [fix] yml redis 환경변수 기본값 수정  #38

--- rebook-chat/src/main/java/com/be/rebook/chat/config/RedisConfig.java
+++ rebook-chat/src/main/java/com/be/rebook/chat/config/RedisConfig.java
@@ -3,8 +3,11 @@ package com.be.rebook.chat.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.be.rebook.chat.service.RedisSubscriber;
 
@@ -12,8 +15,19 @@ import com.be.rebook.chat.service.RedisSubscriber;
 public class RedisConfig {
 
     @Bean
-    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory, 
-                                                 MessageListenerAdapter listenerAdapter) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         return container;
