669bcef7e269ab0c4d5e6ef5f7b1b04899d79248
kimjaehyuk <jhkim0097@gmail.com>
2024-08-13 15:52:59 +0900

    [feat] STOMP 세팅 #38

--- rebook-chat/src/main/java/com/be/rebook/chat/ChatApplication.java
+++ rebook-chat/src/main/java/com/be/rebook/chat/ChatApplication.java
@@ -1,14 +1,10 @@
 package com.be.rebook.chat;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.corundumstudio.socketio.SocketIOServer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -18,13 +14,5 @@ public class ChatApplication {
     public static void main(String[] args) {
         // Spring Boot 애플리케이션 실행
         SpringApplication.run(ChatApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner runner(SocketIOServer server) {
-        return args -> {
-            server.start();
-            Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
-        };
     }
 }
