dc5225d73f385283b4e6e5689a9709ae7810f826
kimjaehyuk <jhkim0097@gmail.com>
2024-08-14 21:16:22 +0900

    feat: Add OAuthMemberInfoFactory class for creating OAuth member information #37

---
+++ rebook-auth/src/main/java/com/be/rebook/auth/oauth/info/OAuthMemberInfoFactory.java
@@ -0,0 +1,17 @@
+package com.be.rebook.auth.oauth.info;
+
+import java.util.Map;
+
+import com.be.rebook.auth.oauth.info.impl.KaKaoOauthMemberInfo;
+
+public class OAuthMemberInfoFactory {
+	public static OAuthMemberInfo getOAuth2MemberInfo(ProviderType providerType, Map<String, Object> attributes) {
+		System.out.println(attributes);
+		switch (providerType) {
+			case KAKAO:
+				return new KaKaoOauthMemberInfo(attributes);
+			default:
+				throw new IllegalArgumentException("Invalid Provider Type.");
+		}
+	}
+}
