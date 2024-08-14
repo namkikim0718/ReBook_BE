5d2138d62e2eb572a534cf431e2d7a07541f3fac
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-14 16:50:14 +0900

    [feat] S3 폴더 선택 추가 및 이미지 삭제 기능 추가

--- rebook-common/src/main/java/com/be/rebook/common/service/S3Service.java
+++ rebook-common/src/main/java/com/be/rebook/common/service/S3Service.java
@@ -2,6 +2,7 @@ package com.be.rebook.common.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.be.rebook.common.type.S3FolderName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,19 +20,37 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String uploadFile(MultipartFile file) throws IOException {
+    /**
+     * 이미지 파일 업로드
+     * file -> 파일 자체를 매개변수로
+     * folderName -> 해당 폴더 기입(Ex. "S3FolderName.PRODUCT")
+     */
+    public String uploadFile(MultipartFile file, S3FolderName folderName) throws IOException {
         // 원본 파일명
         String originalFileName = file.getOriginalFilename();
         // 저장 파일명
         String storeFileName = createStoreFileName(originalFileName);
 
+        // S3에 저장될 전체 경로
+        String fullPath = folderName.getFolderName() + "/" + storeFileName;
+
         // S3에 저장
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
         metadata.setContentLength(file.getSize());
-        amazonS3Client.putObject(bucket, storeFileName, file.getInputStream(), metadata);
+        amazonS3Client.putObject(bucket, fullPath, file.getInputStream(), metadata);
 
         return storeFileName;
+    }
+
+    /**
+     * S3 버킷에서 파일 삭제
+     * folderName -> 해당 폴더이름 기입(Ex."S3FolderName.PRODUCT")
+     * fileName -> 해당 이미지의 storeFileName을 넣으면 됨
+     */
+    public void deleteFile(S3FolderName folderName, String fileName) {
+        String fullPath = folderName.getFolderName() + "/" + fileName;
+        amazonS3Client.deleteObject(bucket, fullPath);
     }
 
     /**
