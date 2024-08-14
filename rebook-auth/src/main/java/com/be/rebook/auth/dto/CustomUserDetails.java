1e3877124424031a7955f1b2a1fa161d0a8f1f39
kimjaehyuk <jhkim0097@gmail.com>
2024-08-14 21:14:13 +0900

    [feat] OAuth 사용을 위한 OAuth2User 추가 #37

--- rebook-auth/src/main/java/com/be/rebook/auth/dto/CustomUserDetails.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/dto/CustomUserDetails.java
@@ -3,15 +3,23 @@ package com.be.rebook.auth.dto;
 import com.be.rebook.auth.entity.Members;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements OAuth2User, UserDetails {
     private final Members members;
+    private Map<String, Object> attributes;
 
     public CustomUserDetails(Members members) {
         this.members = members;
+    }
+
+    public CustomUserDetails(Members members, Map<String, Object> attributes) {
+        this.members = members;
+        this.attributes = attributes;
     }
 
     public String getRoleName() {
@@ -54,5 +62,15 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    @Override
+    public String getName() {
+        return this.members.getUsername();
     }
 }
