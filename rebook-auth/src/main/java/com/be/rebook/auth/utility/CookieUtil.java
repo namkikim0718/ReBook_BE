aca9a2153e8c99ae37af6bd6356466ab14dcac0c
kimjaehyuk <jhkim0097@gmail.com>
2024-08-14 21:15:26 +0900

    [fix] Refresh 토큰 JWTUtil로 분리 및 쿠키 생성 로직 Cookie 유틸로 분리 #37

---
+++ rebook-auth/src/main/java/com/be/rebook/auth/utility/CookieUtil.java
@@ -0,0 +1,20 @@
+package com.be.rebook.auth.utility;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.Cookie;
+
+@Component
+public class CookieUtil {
+
+
+    public Cookie createCookie(String key, String value, int maxAge) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(maxAge);
+        //자바스크립트로 해당 쿠키 접근 못하게
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+}
