bd17f3f94bb4eed5a7400120ce999447ce13faa0
Austin-Choi <austinsupra@gmail.com>
2024-08-14 15:51:06 +0900

    chore : redis 설정

--- rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/config/RedisConfig.java
@@ -10,26 +10,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    //docker 환경에서는
-    //rebook_be-redis-1 으로 host 설정
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.port}")
-    private int port;
-
-    // Redis와의 연결을 위한 connection 생성
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
 
         //Redis 연결
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
 
         //key-value 형태로 직렬화를 수행
         template.setKeySerializer(new StringRedisSerializer());
