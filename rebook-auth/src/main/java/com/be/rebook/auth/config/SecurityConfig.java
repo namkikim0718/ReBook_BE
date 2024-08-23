db04c215f89da5532378491a1bd3f8de916bd8d1
Austin-Choi <austinsupra@gmail.com>
2024-08-24 07:38:13 +0900

    chore : CORS allowed origin 원복

--- rebook-auth/src/main/java/com/be/rebook/auth/config/SecurityConfig.java
+++ rebook-auth/src/main/java/com/be/rebook/auth/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
         http.cors(cors -> cors
                 .configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Collections.singletonList("*"));
+                    configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
